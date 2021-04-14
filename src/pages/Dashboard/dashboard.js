2e953615fe3c4c39dffd6f3b2920b5b0773033fc
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-04-14 15:17:44 -0700

    Home, Dashboard, and Project routes have been added.

---
+++ src/pages/Dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Dashboard() {
+  return (
+    <p>My Dashboard</p>
+  )
+}
+
+export default Dashboard;
---
+++ src/pages/Home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <p>Hello world</p>
+  )
+}
+
+export default Home;
