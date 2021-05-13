16d3262b20cbbe005b0731a457e5ac3e277c66bf
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-05-13 11:45:36 -1000

    added links to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,7 +5,14 @@ function Footer() {
   return (
     <div>
       <Card className="text-center">
-        <Card.Footer className="text-muted">Our App</Card.Footer>
+        <Card.Footer className="text-muted">
+          <p>
+            &copy;
+            <a href="https://www.linkedin.com/in/dominic-j-parker/"> Dominic Parker </a>
+            &
+            <a href="https://www.linkedin.com/in/andrew-bergstrom/"> Andrew Bergstrom</a>
+          </p>
+        </Card.Footer>
       </Card>
     </div>
   );
